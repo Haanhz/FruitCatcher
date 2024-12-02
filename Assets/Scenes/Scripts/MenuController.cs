19507a835e24d0eb25964951d8af0bd212dfacba
lengochuong134 <165996394+lengochuong134@users.noreply.github.com>
2024-12-02 15:22:47 +0700

    hihi

--- Assets/Scenes/Scripts/MenuController.cs
+++ Assets/Scenes/Scripts/MenuController.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class MenuController : MonoBehaviour
 {
+    [Header("Volume Setting")]
+    [SerializeField] private TMP_Text volumeValue = null;
+    [SerializeField] private Slider volumeSlider = null;
+    [SerializeField] private float defaultVolume = 1.0f;
+
+    [Header("Confirmation Prompt")]
+    [SerializeField] private GameObject confirmationprompt = null;
     public void Single()
     {
         SceneManager.LoadScene(1);
@@ -14,5 +22,40 @@ public class MenuController : MonoBehaviour
     public void Multiple()
     {
         SceneManager.LoadScene(2);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void SetVolume(float volume)
+    {
+        AudioListener.volume = volume;
+        volumeValue.text = volume.ToString("0.0");
+    }
+
+    public void VolumeApply()
+    {
+        PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
+        StartCoroutine(ConfirmationBox());
+    }
+
+    public void ResetButton(string MenuType)
+    {
+        if (MenuType == "Audio")
+        {
+            AudioListener.volume = defaultVolume;
+            volumeSlider.value = defaultVolume;
+            volumeValue.text = defaultVolume.ToString("0.0");
+            VolumeApply();
+        }
+    }
+
+    public IEnumerator ConfirmationBox()
+    {
+        confirmationprompt.SetActive(true);
+        yield return new WaitForSeconds(0);
+        confirmationprompt.SetActive(false);
     }
 }
